df6ec825a6f0c1fd5050319dbadc4f890f46af9e
Clay Carden <clayton.a.carden@gmail.com>
2023-03-24 15:09:08 -0500

    Finished Operator Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace OperatorExercise
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            Console.WriteLine($"What is the radius of your circle?");
+            var userInput = Console.ReadLine();
+            var radius = double.Parse(userInput);
+            Console.WriteLine($"The area of circle with radius of {userInput} is {AreaOfCircle(radius)}");
+
+
         }
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * radius * Math.Pow(radius, 1);
+            return area;
+            
+
+        }
+     
     }
 }
